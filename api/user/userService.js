f623c8f692aa85124515fc8c468b2ab6dc99e43b
JDK <ehdrjf2062@gmail.com>
2022-09-25 20:43:38 +0900

    Chore: 유저API 주석

--- api/user/userService.js
+++ api/user/userService.js
@@ -2,11 +2,13 @@ const userDAO = require("./userDAO");
 const { BadRequestError } = require("../../modules/error");
 const jwt = require("../../modules/jwt");
 
+//유저생성
 async function createUser(user) {
   const email = user.email;
   await userDAO.createByEamil(email);
 }
 
+//토큰발급
 async function creatToken(user) {
   const email = user.email;
   const validedUser = await userDAO.findOneByEamil(email);

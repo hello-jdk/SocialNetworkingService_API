d67ec56682df5f183b6a0738ddb913623ee013d8
JDK <ehdrjf2062@gmail.com>
2022-09-26 22:18:23 +0900

    Feat: 게시글 라우터

--- api/router.js
+++ api/router.js
@@ -1,7 +1,14 @@
 const router = require("express").Router();
 const userController = require("./user/userController");
+const boardController = require("./board/boardController");
 
 router.post("/users/signup", userController.signUp);
 router.post("/users/login", userController.login);
+
+router.get("/boards/:id", userController.selectOne);
+router.post("/boards", userController.create);
+router.put("/boards", userController.update);
+router.delete("/boards", userController.delete);
+//router.get("/boards/list")
 
 module.exports = { router };

ad4709ae666586d0417d4d57c63ff5910275ac17
JDK <ehdrjf2062@gmail.com>
2022-09-28 12:39:42 +0900

    Modify: jwt 에러처리 핸들링

--- middleware/auth.js
+++ middleware/auth.js
@@ -7,21 +7,21 @@ const authUtil = {
     var token = req.headers.token;
     // 토큰 없음
     if (!token) {
-      throw new BadRequestError("토큰이 비어있습니다.");
+      return next(new BadRequestError("토큰이 비어있습니다."));
     }
     // 디코드
     const user = await jwt.verify(token);
     // 토큰 만료
     if (user === TOKEN_EXPIRED) {
-      throw new UnauthorizedError("토큰이 만료되었습니다.");
+      return next(new UnauthorizedError("토큰이 만료되었습니다."));
     }
     // 토큰 유효하지 않음
     if (user === TOKEN_INVALID) {
-      throw new UnauthorizedError("토큰이 유효하지않습니다.");
+      return next(new UnauthorizedError("토큰이 유효하지않습니다."));
     }
     // DB 정보와 다름
     if (user.id === undefined) {
-      throw new UnauthorizedError("유저 정보가 일치하지 않습니다.");
+      return next(new UnauthorizedError("유저 정보가 일치하지 않습니다."));
     }
     req.body.user = user;
     next();
